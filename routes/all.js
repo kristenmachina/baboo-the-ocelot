7d6ed4fe67c7c5a91a73212a4ad69dce1895d89a
Kristen Machina <kristenmachina@gmail.com>
2014-10-13 11:05:27 -0400

    misc updates

--- routes/all.js
+++ routes/all.js
@@ -1,11 +1,13 @@
+var express = require('express');
+var about_routes = express.Router();
+exports.about = about_routes;
 
-/*
- * GET home page.
- */
-
-exports.about = function(req, res){
+about_routes.use('/', function(req, res){
   res.render('about', { title: 'About' });
-};
+});
+about_routes.use('/characters', function(req, res) {
+  res.render('about/characters', { title: 'Characters' });
+  });
 
 exports.characters = function(req, res){
   res.render('about', { title: 'Characters' });
