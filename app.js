3b07b4d286274f8c06e8607b8cac9ddfb5611455
Kristen Machina <kristenmachina@gmail.com>
2014-10-13 11:22:15 -0400

    Updated to express 4.0

--- app.js
+++ app.js
@@ -4,29 +4,22 @@
  */
 
 var express = require('express');
-var routes = require('./routes/all');
-var comic = require('./routes/comic');
+var favicon = require('serve-favicon');
 var http = require('http');
+var logger = require('morgan');
 var path = require('path');
+
+var comic = require('./routes/comic');
+var routes = require('./routes/all');
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', comic.newest);
 app.get('/about', routes.about);
@@ -36,6 +29,32 @@ app.get('/archive', routes.archive);
 app.get('/comic', comic.newest);
 app.get(/^\/comic\/([0-9]+)-([0-9]{2,2})$/, comic.get);
 
-http.createServer(app).listen(app.get('port'), function(){
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if ('development' == app.get('env')) {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(app.get('port'), function() {
   console.log('Express server listening on port ' + app.get('port'));
 });
+
--- routes/all.js
+++ routes/all.js
@@ -1,13 +1,15 @@
 var express = require('express');
+
 var about_routes = express.Router();
 exports.about = about_routes;
 
 about_routes.use('/', function(req, res){
   res.render('about', { title: 'About' });
 });
+
 about_routes.use('/characters', function(req, res) {
   res.render('about/characters', { title: 'Characters' });
-  });
+});
 
 exports.characters = function(req, res){
   res.render('about', { title: 'Characters' });
